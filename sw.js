2f8ab6292ef3858c5a186e445d8b90ecb39a9221
Lucio Boxall <lucio.boxall@davinci.edu.ar>
2022-12-05 21:51:47 -0300

    a´pp

--- sw.js
+++ sw.js
@@ -1,3 +1,25 @@
+const CACHE_NAME = 'cache-1';
 self.addEventListener('install', function(e){
     console.log(e);
+    const cache = caches.open(CACHE_NAME).then( cache => {
+        return cache.addAll([
+            'app.js',
+            'index.html',
+            'icons',
+            'style.css'
+        ])
+    })
+    e.waitUntil( cache );
+})
+
+self.addEventListener('fetch', e => {
+    const respuestaCache = caches.match(e.request).then( res=> {
+        if(res) {
+            res;
+        } else {
+            return fetch(e.request)
+        }
+    })
+
+    e.respondWith(respuestaCache)
 })
